f95100ed1520d150405d9a3917060465260f8031
Laura Caroselli <lcaroselli@outlook.com>
2017-05-10 15:40:26 -0600

    Set reset button to generate new random number

--- script.js
+++ script.js
@@ -1,37 +1,67 @@
 //Global Variables
 
 var submitGuessButton = document.querySelector("#submit-your-guess");
-//make a variable called submitGuessButton and assign it to the element in the document with the id of submit-your-guess (input for the "Guess" button)
+//"Guess" Button (variable submitGuessButton assigned to the element with id of submit-your-guess)
 
 var clearGuessButton = document.querySelector("#clear-your-guess")
-//make a variable called clearGuessButton and assign it to the element in the document with the id of clear-your-guess (input for the "Clear" button)
+//"Clear" Button (variable clearGuessButton assigned to the element with id of clear-your-guess)
 
 var resetButton = document.querySelector("#reset-field")
-//make a variable called resetButton and assign it to the element in the document with the id of reset-field
+//"Reset" Button (variable resetButton assigned to the element with the id of reset-field)
 
 var randomGuess = document.querySelector("#your-guess");
-//make a variable called randomGuess and assign it to the element in the document with the id of your-guess (input for the field you enter a number)
+//Enter your guess (variable randomGuess assigned to the element with the id of your-guess)
 
-var randomNum = Math.floor((Math.random() * 100) +1);
-//make a variable called randomNum and assign it to a Math object which will create & store random numbers between 1 and 100
+var randomNum = generateRandomNum();
+//Random number generated (variable randomNum assigned to a Math object which will generate a random number between 1 and 100)
 //MAXIMUM AND MINIMUM NOT WORKING
 
 var displayNumber = document.querySelector(".display");
-//make a variable called displayNumber and assign it to the element(s) in the document with the class of "display" (this is the <p> tag displaying the number)
+//Displayed number (variable displayNumber assigned to the element with the class of "display" - this is the <p> tag displaying the guessed number on the page)
 
 var displayMessage = document.querySelector(".user-message");
-//make a variable called displayMessage and assign it to the element(s) in the document with the class of "user-message" (this is the <p> tag displaying the message to be given back to the user) -- NEED TO WRITE CONDITIONAL TO CHANGE THE TEXT OF THIS MESSAGE...
+//Displayed user message (variable displayMessage assigned to the element with the class of "user-message" - this is the <p> tag displaying the message to be given back to the user upon guessing a number)
 
 var displayLastGuess = document.querySelector("#top-message");
+//"Your last guess was"
 
 //var el = document.getElementByClassName("display");
 //el.innerHTML = '<p>' + randomNum + '</p>';
 //nothing has been done here yet -- please see page 134 for more information
 
+var min = 1;
+var max = 100;
+
+
 
 //Functions
-//Upon clicking "Guess", the user's guess is displayed on the screen, as well as a message on whether the guessed number is too high or too low
+//Clear field
+function clearField () {
+    displayMessage.textContent = (" ");
+    randomGuess.value = (null);
+    //DISABLE BUTTONS...
+    //clearGuessButton.disabled = true;
+    //resetButton.disabled = true;
+    return;
+}
+
+//Generate a random number
+function generateRandomNum () {
+  return Math.floor((Math.random() * 100) +1);
+}
+
+//Reset field
+function resetField () {
+  displayMessage.textContent = (" ");
+  displayNumber.textContent = (" ");
+  displayLastGuess.textContent = ("Play again?");
+  return;
+}
+
+//Events
+//Clicking "Guess" - user's guess is displayed on the screen, as well as a message on whether the guessed number is too high or too low
 submitGuessButton.addEventListener('click', function() {
+  validateNumber();
   displayLastGuess.textContent = ("Your last guess was");
   displayNumber.textContent = randomGuess.value;
   if (parseInt(randomGuess.value) > randomNum) {
@@ -41,26 +71,23 @@ submitGuessButton.addEventListener('click', function() {
   } else if (parseInt(randomGuess.value) === randomNum){
     return displayMessage.innerText = ("BOOM!");
   } else {
-    return displayMessage.innerText = ("...");
-  }
+  displayLastGuess.innerText = (" ");
+  return displayMessage.innerText = ("That's not a number, dude...");
+}
 });
 
-//Upon clicking "Clear", the input field is cleared and the text content is cleared
-clearGuessButton.addEventListener ('click', function() {
-  displayNumber.textContent = " ";
-  displayMessage.textContent = " ";
-  displayLastGuess.textContent = " "; //MAKE IT SO LAST GUESS STICKS AROUND EVEN AFTER CLEARING
-  randomGuess.value = " ";
+
+
+//Clicking "Clear" - function clearField is ran
+clearGuessButton.addEventListener ('click', function () {
+  clearField();
   });
 
-//Reset button to reset field
+//Clicking "Reset" - function resetField is ran and a new random number is generated
 resetButton.addEventListener ('click', function() {
-  
+  resetField();
+  randomNum = generateRandomNum();
 });
-
-//PHASE 1 REMAINING:
-//Function to reset the game - ZERO PHASE !!! IMPORTANT
-//Keep last guess around while clearing input field? Until reset
 
 
 /*AT PHASE 2:
